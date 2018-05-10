adeec1d22c26f37fcdbe6af4b6bf8762549a5a3b
Tareq <tmasri58@gmail.com>
2018-05-10 13:49:11 -0400

    when hide menu button, double click anywhere on screen to make button visible again

--- js/menu/menuVisibility.js
+++ js/menu/menuVisibility.js
@@ -1,27 +1,22 @@
 
-var v = 0;
-var a = 0;
-
-
 function changeH(i) {
    if (i == 1) {
-      // turn 12 to gray (remove selection)
+      // turn visible to gray (remove selection)
       $("#on").removeClass('on');
       $("#on").addClass('chosen');
-      // turn 24 to blue (add selection)
+      // turn hidden to blue (add selection)
       $("#off").removeClass('chosen');
       $("#off").addClass('on');
-
+      // hide the menu button
       $("#menu-button").addClass("hide");
    } else {
-      // turn 12 to blue (add selection)
+      // turn visible to blue (add selection)
       $("#on").removeClass('chosen');
       $("#on").addClass('on');
-      // turn 24 to gray (remove selection)
+      // turn hidden to gray (remove selection)
       $("#off").removeClass('on');
       $("#off").addClass('chosen');
-
+      // show the menu button
       $("#menu-button").removeClass("hide");
    }
-   // v = i;
 }
